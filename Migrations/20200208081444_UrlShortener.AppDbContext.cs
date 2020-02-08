128573d977d2179a3c863a95f6e068df0acad137
nika shahabi <nikashahabi@aut.ac.ir>
2020-02-08 11:47:09 +0330

    Migrations added

---
+++ Migrations/20200208081444_UrlShortener.AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UrlShortener.Migrations
+{
+    public partial class UrlShortenerAppDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "urlconverter",
+                columns: table => new
+                {
+                    shortUrl = table.Column<string>(nullable: false),
+                    longUrl = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_urlconverter", x => x.shortUrl);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "urlconverter");
+        }
+    }
+}
